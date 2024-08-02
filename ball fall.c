7cdb2862f789ccd4161a77c7a10387ad0ab3b7cf
Gaurab <159932313+gaurab-arl@users.noreply.github.com>
2024-08-02 12:57:00 +0545

    Update ball fall.c

--- ball fall.c
+++ ball fall.c
@@ -9,8 +9,8 @@ const int screenHeight = 480;
 
 // Paddle dimensions
 const int paddleWidth = 100;
-const int paddleHeight = 10;
-const int paddleSpeed = 20;
+const int paddleHeight = 20;
+const int paddleSpeed = 50;
 
 // Ball dimensions
 const int ballRadius = 10;
@@ -18,13 +18,15 @@ int ballSpeed = 5;
 
 // Function prototypes
 void drawPaddle(int x);
-void drawBall(int x, int y);
+void drawBall(int x, int y, bool isHovered);
 void displayScore(int score, int level);
+void showMainMenu();
+void drawBorder();
 
 int main()
 {
-    int gr = DETECT, gm;
-    initgraph(&gr, &gm, "");
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
 
     int paddleX = screenWidth / 2 - paddleWidth / 2;
     int ballX = rand() % (screenWidth - 2 * ballRadius) + ballRadius;
@@ -34,19 +36,29 @@ int main()
     int level = 1;
     int consecutiveHits = 0;
     char ch;
+    bool isHovered = false;
 
     // Seed random number generator
     srand(time(0));
+
+    // Display the main menu
+    showMainMenu();
+
+    // Wait for a key press to start the game
+    getch();
 
     while (true) {
         // Clear screen
         cleardevice();
 
+        // Draw the border
+        drawBorder();
+
         // Draw paddle
         drawPaddle(paddleX);
 
-        // Draw ball
-        drawBall(ballX, ballY);
+        // Draw ball with hover effect
+        drawBall(ballX, ballY, isHovered);
 
         // Display score and level
         displayScore(score, level);
@@ -62,6 +74,7 @@ int main()
             ballX = rand() % (screenWidth - 2 * ballRadius) + ballRadius;
             score += 1 + consecutiveHits; // Award bonus points for consecutive hits
             consecutiveHits++;
+            isHovered = true; // Activate hover effect
 
             if (score % 10 == 0) {
                 level++;
@@ -72,6 +85,8 @@ int main()
             outtextxy(screenWidth / 2 - 50, screenHeight / 2, "Game Over!");
             getch();
             break;
+        } else {
+            isHovered = false; // Deactivate hover effect if not hitting the paddle
         }
 
         // Ball hits the left or right wall
@@ -107,17 +122,35 @@ void drawPaddle(int x)
     bar(x, screenHeight - paddleHeight, x + paddleWidth, screenHeight);
 }
 
-// Draw the ball
-void drawBall(int x, int y)
+// Draw the ball with hover effect
+void drawBall(int x, int y, bool isHovered)
 {
-    setfillstyle(SOLID_FILL, RED);
+    setfillstyle(SOLID_FILL, isHovered ? YELLOW : RED); // Change color on hover
     fillellipse(x, y, ballRadius, ballRadius);
 }
 
 // Display the score and level
 void displayScore(int score, int level)
 {
+    settextstyle(BOLD_FONT, HORIZ_DIR, 3); // Set larger font size
     char scoreText[30];
     sprintf(scoreText, "Score: %d  Level: %d", score, level);
-    outtextxy(10, 10, scoreText);
+    outtextxy(20, 20, scoreText);
 }
+
+// Show the main menu
+void showMainMenu()
+{
+    cleardevice();
+    settextstyle(BOLD_FONT, HORIZ_DIR, 4);
+    outtextxy(screenWidth / 2 - 160, screenHeight / 2 - 60, "Welcome to Paddle Game");
+    outtextxy(screenWidth / 2 - 160, screenHeight / 2, "Press any key to start");
+}
+
+// Draw the border
+void drawBorder()
+{
+    setcolor(WHITE);
+    rectangle(0, 0, screenWidth - 1, screenHeight - 1);
+}
+
